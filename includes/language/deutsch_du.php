3887da899bc73c3095ee2d244e2326bdd04ec522
Ole Sondermann <ole@creativefx.de>
2018-01-22 23:59:51 +0100

    Create deutsch_du.php
    German informal

---
+++ includes/language/deutsch_du.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @package HelpDeskZ
+ * @website: http://www.helpdeskz.com
+ * @community: http://community.helpdeskz.com
+ * @author Ole "OJay" - https://www.noxsports.net
+ * @since 1.0.0
+ */
+$LANG['HOME'] = 'Start';
+$LANG['MY_TICKETS'] = 'Meine Tickets';
+$LANG['SUBMIT_A_TICKET'] = 'Ticket verfassen';
+$LANG['KNOWLEDGEBASE'] = 'Knowledgebase';
+$LANG['NEWS'] = 'Neuigkeiten';
+$LANG['ACCOUNT_LOGIN'] = 'Account Login';
+$LANG['LASTEST_UPDATES'] = 'Letzten Updates';
+$LANG['YOUR_EMAIL_ADDRESS'] = 'Deine E-Mailadresse';
+$LANG['YOUR_PASSWORD'] = 'Dein Passwort';
+$LANG['REMEMBER_ME'] = 'Erinnern';
+$LANG['LOST_PASSWORD'] = 'Passwort vergessen';
+$LANG['LOGIN'] = 'Login';
+$LANG['ACCOUNT'] = 'Account';
+$LANG['MY_PROFILE'] = 'Mein Profil';
+$LANG['PREFERENCES'] = 'Preferences';
+$LANG['CHANGE_PASSWORD'] = 'Passwort &auml;ndern';
+$LANG['LOGOUT'] = 'Logout';
+$LANG['VIEW_ALL_NEWS'] = 'Alle Neuigkeiten anzeigen.';
+$LANG['POSTED_BY_ON'] = 'Verfasst von %author% am %date%';
+$LANG['OLDEST_POSTS'] = '&Auml;ltere Beitr&auml;ge';
+$LANG['NEWEST_POSTS'] = 'Neuste Beiträge';
+$LANG['MOST_POPULAR_ARTICLES'] = 'Popul&auml;rste Artikel';
+$LANG['NEWEST_ARTICLES'] = 'Neuste Artikel';
+$LANG['MORE_TOPICS'] = 'Weitere Themen';
+$LANG['FILE_NOT_FOUND'] = 'Datei nicht gefunden.';
+$LANG['SEARCH'] = 'Suche';
+$LANG['SEARCH_IN_KNOWLEDGEBASE'] = 'Suchen in Knowledgebase';
+$LANG['SEARCH_RESULTS'] = 'Suchresultate';
+$LANG['SEARCH_NOT_FOUND'] = 'Wir haben nichts relevantes zu deiner Suche gefunden';
+$LANG['ENTER_EMAIL_RECOVER_PASSWORD'] = 'Bitte gebe deine E-Mailadresse ein und du bekommst eine E-Mail mit einem Link um dein Passwort zur&uuml;ck zu setzen.';
+$LANG['ACCOUNT_INFORMATION'] = 'Account Informationen';
+$LANG['EMAIL'] = 'E-mail';
+$LANG['CAPTCHA_VERIFICATION'] = 'CAPTCHA Verification';
+$LANG['ENTER_TEXT_CAPTCHA'] = 'Bitte gib den Text den du siehst in das Textfeld unterhalb ein.';
+$LANG['SUBMIT'] = 'Absenden';
+$LANG['CANT_FIND_SOLUTION_SUBMIT_TICKET'] = 'Wenn du keine Antwort in der Knowlagebase finden kannst, sende ein Ticket an die bestimmte Abteilung.';
+$LANG['SELECT_DEPARTMENT_TICKET'] = 'Bitte, w&auml;hle eine Abteilung aus an der dein Ticket gehen soll.';
+$LANG['CSRF_ERROR'] = 'Es ist ein Problem aufgetretten; bitte versuche es erneut.';
+$LANG['DEPARTMENTS'] = 'Abteilungen';
+$LANG['NEXT'] = 'Nächstes';
+$LANG['INVALID_EMAIL_OR_PASSWORD'] = 'Falsche E-Mailadresse oder Passwort';
+$LANG['YOUR_TICKET_DETAILS'] = 'Deine Ticketdetails';
+$LANG['ENTER_TICKET_DETAILS'] = 'Schreibe dein Anliegen bitte in die folgenden Felder. Wenn du ein Problem schildern willst, sei bitte genau wie möglich.';
+$LANG['GENERAL_INFORMATION'] = 'Allgemeine Informationen';
+$LANG['FULL_NAME'] = 'Name';
+$LANG['PRIORITY'] = 'Dringlichkeit';
+$LANG['YOUR_MESSAGE'] = 'Deine Nachricht';
+$LANG['SUBJECT'] = 'Betreff';
+$LANG['ATTACHMENTS'] = 'Anhang';
+$LANG['INVALID_CAPTCHA_CODE'] = 'ERROR: inkorrekter Captcha-Code';
+$LANG['INVALID_EMAIL_ADDRESS'] = 'ERROR: Bitte füge eine echte E-mailadresse ein';
+$LANG['ONE_REQUIRED_FIELD_EMPTY'] = 'Eines der Pflichtfelder ist leer';
+$LANG['ERROR_UPLOADING_A_FILE'] = 'Es ist ein Fehler beim Hochladen deiner Datei aufgetretten.';
+$LANG['INVALID_FILE_EXTENSION'] = 'ungültige Datei';
+$LANG['FILE_NOT_ALLOWED'] = 'ungültige Dateiendung';
+$LANG['FILE_IS_BIG'] = 'Deine Datei ist zu groß. Maximum size is %size%';
+$LANG['REQUEST_RECEIVED'] = 'Dein Ticket wurde abgesendet';
+$LANG['REQUEST_RECEIVED_RESPONSE'] = 'Wir haben deine Anfrage bekommen und unser Team wird sich schnellst möglich bei dir melden. Du kannst dich jederzeit im System anmelden und den Status deines Tickets kontrollieren oder auf die Antwort per E-Mail warten.';
+$LANG['TICKET_ID'] = 'Ticket ID';
+
+$LANG['WE_SENT_YOUR_NEW_PASSWORD'] = 'Wir haben dir ein neues Passwort gesendet.';
+$LANG['EMAIL_WAS_NOT_FOUND'] = 'E-Mailadresse wurde nicht gefunden.';
+$LANG['SEARCH_TICKET_ID'] = 'Suche: Ticket ID';
+$LANG['VIEW_TICKETS'] = 'Tickets Anzeigen';
+$LANG['VIEW_TICKETS_DESCRIPTION'] = 'Die folgenden Tickets sind welche die du in der Vergangenheit gesendet hattest. Klick auf den Betreff um dir das Ticket anzuschauen.';
+$LANG['LAST_UPDATE'] = 'Letztes Update';
+$LANG['DEPARTMENT'] = 'Abteilung';
+$LANG['STATUS'] = 'Status';
+$LANG['VIEW_TICKET_ID'] = 'Ticket #%id% Anzeigen';
+$LANG['CREATED'] = 'Erstellt';
+$LANG['UPDATED'] = 'Aktualisiert';
+$LANG['TICKET_IS_CLOSED'] = 'Ticket ist geschlossen.';
+$LANG['ADD_REPLY'] = 'Antworten';
+$LANG['YOUR_REPLY_SENT'] = 'Deine Antwort wurde erfolgreich gesendet.';
+$LANG['USER'] = 'User';
+$LANG['STAFF'] = 'Staff';
+$LANG['POSTED_ON'] = 'Geschrieben am'; //here
+
+$LANG['TICKET_NOT_FOUND_OR_PERMISSION'] = 'Sorry, we couldn\'t find the ticket you requested or you do not have permission to view it.';
+$LANG['TICKET_NOT_FOUND'] = 'Ticket wurde nicht gefunden.';
+$LANG['YOUR_PROFILE_UPDATED'] = 'Dein Profil wurde aktualisiert.';
+$LANG['UPDATE'] = 'Update';
+$LANG['YOUR_PREFERENCES_SAVED'] = 'Deine Präferenzen wurden gespeichert';
+$LANG['GENERAL_OPTIONS'] = 'Allgemeine Einstellungen';
+$LANG['TIMEZONE'] = 'Zeitzone';
+$LANG['DEFAULT_TIMEZONE'] = 'standart Zeitzone';
+$LANG['PASSWORD_UPDATED'] = 'Passwort aktualisiert.';
+$LANG['PASSWORD_DETAILS'] = 'Passwortdetails';
+$LANG['EXISTING_PASSWORD'] = 'Aktuelles Passwors';
+$LANG['NEW_PASSWORD'] = 'Neues Passwort';
+$LANG['CONFIRM'] = 'best&auml;tigen';
+$LANG['EXISTING_PASSWORD_INCORRECT'] = 'Dein aktuelles Passwort ist nicht korrekt.';
+$LANG['NEW_PASSWORDS_DO_NOT_MATCH'] = 'Dein neues Passwort stimmt nicht überein-';
+$LANG['EMAIL_ASSOCIATED_OTHER_ACCOUNT'] = 'Die E-Mailadresse wird bereits verwendet';
+$LANG['WE_ARE_PERFORMING_MAINTENANCE'] = 'Wir arbeiten zur Zeit an unserem System. Wir bitten um Entschuldigung.';
+$LANG['MAINTENANCE_MODE'] = 'Wartungsmodus';
+$LANG['PAGE_NOT_FOUND'] = 'Page not found - 404';
+$LANG['CLICK_TO_BACK_HELPDESK'] = 'Klick um zum Helpdesk zur&uuml;ck zu kehren';
+$LANG['OPEN'] = 'Offen';
+$LANG['AWAITING_REPLY'] = 'Erwartet Antwort';
+$LANG['IN_PROGRESS'] = 'In Arbeit';
+$LANG['ANSWERED'] = 'Beantwortet';
+$LANG['CLOSED'] = 'Geschlossen';
+?>
